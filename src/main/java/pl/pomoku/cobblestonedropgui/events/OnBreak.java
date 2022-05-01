12beeead8ba42fa5b4c521e9e9822a7718085d80
Hubert Rybicki <hubert.rybicki.hr@gmail.com>
2022-05-01 22:20:55 +0200

    Dodanie funkcji automatycznego wyłączenia dropu "emeraldu" do ekwipunku, gdy nie ma nie niego miejsca.

--- src/main/java/pl/pomoku/cobblestonedropgui/events/OnBreak.java
+++ src/main/java/pl/pomoku/cobblestonedropgui/events/OnBreak.java
@@ -137,7 +137,15 @@ public class OnBreak implements Listener {
                     }
                 }else {
                     if (plugin.getConfig().getString(uuid + ".emerald") == "true") {
-                        p.getInventory().addItem(new ItemStack(Material.EMERALD, itemAmoundRandom(1,2)));
+                        if(isInventoryFull(p, Material.EMERALD, 64) == false) {
+                            p.getInventory().addItem(new ItemStack(Material.EMERALD, itemAmoundRandom(1, 2)));
+                        }else {
+                            p.sendMessage(" ");
+                            p.sendMessage(ChatColor.YELLOW + "Nie masz miejsca w EQ! " + ChatColor.GRAY + "Drop do EQ zostal " + ChatColor.RED + "wylaczony" + ChatColor.GRAY + ", jezeli chcesz go wlaczyc oporznij ekwipunek.");
+                            p.sendMessage(" ");
+                            plugin.getConfig().set(uuid + ".eq", "true");
+                            plugin.saveConfig();
+                        }
                     }
                 }
             }else if(percentChance(0.4)) { //11%
